128c0b78253ed580e0284b9e14073eea8e8e59c6
alexannekk <alex.annecchini@gmail.com>
2024-07-18 04:11:37 +0200

    calcolo linea e fermata_due_to_search
    - modificato file json: ora si memorizzano solo gli id identificativi dei valori principali; aggiunto dizionario di buffer; ora il journey pattern unbound si chiama così e non "".
    - ottimizzate e semplificate le funzioni per calcolo linea e fermate: ora dovrebbero essere leggermente più chiari e veloci (40s la linea e 1m40 nel peggiore dei casi la seconda)
    -cambiata un pò di nomenclatura generale

--- Tap&Go/infomobility.py
+++ Tap&Go/infomobility.py
@@ -6,7 +6,7 @@ import json
 # SEZIONE FILEs
 ############################################################################################
 
-# Apre il lettura o scittura i file JSON
+# Apre il lettura o scrittura i file JSON
 def open_json(mode, filename, data=""):
     if mode: # 1 lettura
         with open(filename, "r") as jsonfile:
@@ -23,62 +23,37 @@ def open_json(mode, filename, data=""):
 ############################################################################################
 
 
-# Crea una lista di codici fermata appartenenti ad una stessa direzione 
-def create_list_ids(lst, direction):
-    list_ids = []
-    for item in lst:
-        if item["direction"] == direction:
-            sublist = item["stops"]
-            list_ids.extend(sub["id"] for sub in sublist)
-    return list_ids
-
-
-# Trova tutte le linee che condividono la stessa tratta parziale
-def find_lines(stops, root, ns):
-    lines = []
-    for sjp in root.findall(f".//{ns}ServiceJourneyPattern"):
-        stops_ref = {r.get("ref") for r in sjp.findall(f".//{ns}ScheduledStopPointRef")}
-        if set(stops) <= set(stops_ref):
-            lines_ref = sjp.find(f".//{ns}LineRef")
-            lines.append(lines_ref.get("ref"))
-    return lines
-
-
 # Trova il bus corrente e ne memorizza il valore
 def calculateLine(file):
     params = open_json(1, file)
     netex = params["netex"]["netex_file_path"] + params["netex"]["netex_file_name"]
     ns = params["netex"]["namespace"]
 
-    journey_patterns = params["infomobility"]["JourneyPattern"]
+    journeys = params["infomobility"]["journeys"]
     
     # Carica il file XML
     root = ET.parse(netex)
 
-    linee_in, linee_out, linee_un = [], [], []
-
-    stops_inbound_id = create_list_ids(journey_patterns, "inbound")
-    if stops_inbound_id:
-        linee_in= find_lines(list(set(stops_inbound_id)), root, ns)
-    stops_outbound_id = create_list_ids(journey_patterns, "outbound")
-    if stops_outbound_id:
-        linee_out= find_lines(list(set(stops_outbound_id)), root, ns)
-    stops_unbound_id = create_list_ids(journey_patterns, "")
-    if stops_unbound_id:
-        linee_un= find_lines(list(set(stops_unbound_id)), root, ns)
-
-    linee = linee_in + linee_out + linee_un
-    unique_lines = list(set(linee))
-    if len(unique_lines) == 1:
-        linea = root.find(f".//{ns}Line[@id='" + unique_lines[0] + "']")
-
-        params["infomobility"]["Line"]["id"] = linea.get("id")
-        params["infomobility"]["Line"]["name"] = linea.findtext(f"{ns}Name")
-        journey_patterns = []
+    # Trova tutte le linee che condividono la stessa tratta parziale a partire dalle fermate trovate
+    # Una tratta è tra quelle papabili se include tutte le fermate memorizzate fin ora
+    linee_id = []
+    for journey in journeys:
+         if journey["direction"] == "unbound":
+            stops_id = journey["stops"]
+    for sjp in root.findall(f".//{ns}ServiceJourneyPattern"):
+        stops_sjp = {r.get("ref") for r in sjp.findall(f".//{ns}ScheduledStopPointRef")}
+        if set(stops_id) <= set(stops_sjp):
+            #lines_ref = sjp.find(f".//{ns}LineRef")
+            linee_id.append(sjp.find(f".//{ns}LineRef").get("ref"))
+    
+    unique_lines_id = list(set(linee_id))
+    if len(unique_lines_id) == 1:
+        params["infomobility"]["line_id"] = unique_lines_id[0]
+        journeys = []
         open_json(0, file, params)
 
-    elif len(unique_lines) == 0:
-        journey_patterns = []
+    elif len(unique_lines_id) == 0:
+        journeys = []
         open_json(0, file, params)
 
 
@@ -87,7 +62,8 @@ def calculateLine(file):
 ############################################################################################
 
 
-# Funzione per calcolare la distanza tra due punti geografici
+# Calcolare la distanza tra due punti geografici
+# Formula di Haversine
 def calculate_distance(lat1, lon1, lat2, lon2):
     raggio_terra = 6371000  # Raggio medio della Terra in metri
     delta_lat = math.radians(lat2 - lat1)
@@ -98,30 +74,11 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     return distanza
 
 
-def search_dict(lst, value):
-    for dict in lst:
-        if dict["direction"] == value:
-            return dict
-
-
-# Funzione per scoprire la fermata al quale il bus si ferma 
-def calculateStop_due_to_search(file):
+# Trova le fermate più vicine al punto di riferimento
+def neighborhooded_stops(file):
     params = open_json(1, file)
-
     netex = params["netex"]["netex_file_path"] + params["netex"]["netex_file_name"]
     ns = params["netex"]["namespace"]
-
-    journey_patterns = params["infomobility"]["JourneyPattern"]
-    unbound = search_dict(journey_patterns, "")
-    if not unbound:
-        journey_patterns.append(
-            {
-                "id": "",
-                "direction": "",
-                "stops": []
-            }
-        )
-
 
     lat = params["position_rt"]["latitude"]
     lon = params["position_rt"]["longitude"]
@@ -130,78 +87,118 @@ def calculateStop_due_to_search(file):
     # Carica il file XML
     root = ET.parse(netex)
 
-    # Trova tutte le fermate degli autobus
     fermate = root.findall(f".//{ns}ScheduledStopPoint")
-
-    # Trova le fermate più vicine
-    fermate_vicine = []
+    nearby = []
     for fermata in fermate:
         lat_stop = float(fermata.findtext(f".//{ns}Latitude"))
         lon_stop = float(fermata.findtext(f".//{ns}Longitude"))
         distanza = calculate_distance(lat, lon, lat_stop, lon_stop)
         if distanza <= delta:
-            fermate_vicine.append({"id": fermata.get("id"), "name": fermata.findtext(f"{ns}ShortName")})
-
-    # Se trova 1 fermata la si memorizza (sperando sia giusta)
-    if len(fermate_vicine) == 1:
-        for idx in range(len(journey_patterns)):
-            if journey_patterns[idx]["direction"] == "":
-                journey_patterns[idx]["stops"].append(fermate_vicine[0])
-
-    # Se trova più fermate si cerca di capire il verso del bus
-    # le prossime coordinate gps ce lo diranno
-    # se il "link to" si allontana mentre ad uno si avvicina, si più filtrare
-    #elif len(fermate_vicine) > 1:
-        #for idx in range(len(fermate_vicine)):     
-        #    break  
-
-    # Se non si trovano fermate  si cerca la fermata più vicina a noi
-    # da lì cercare di capire se è la nostra meta oppure veniamo da lì
-    #else:
-        #fermate = root.findall(f".//{ns}ScheduledStopPoint")
-        #for fermata in fermate:
-            #stop_id = fermata.get("id")
-            #stop_name = fermata.findtext(f"{ns}Name")
-            #lat_stop = float(fermata.findtext(f".//{ns}Latitude"))
-            #lon_stop = float(fermata.findtext(f".//{ns}Longitude"))
-            #distanza = calculate_distance(lat, lon, lat_stop, lon_stop)
-            #if distanza < min_distance:
-                #min_distance = distanza
-                #closest_stop = {"id": stop_id, "name": stop_name}
-    
-    open_json(0, file, params)
-    # Se comunque resta il dubbio, non si memorizza nessuna fermata
+            nearby.append(fermata.get("id"))
+    return nearby
 
 
 # Funzione per scoprire la fermata al quale il bus si ferma 
-def calculateStop_due_to_validation(file):
+def calculateStop_due_to_search(file):
     params = open_json(1, file)
 
     netex = params["netex"]["netex_file_path"] + params["netex"]["netex_file_name"]
     ns = params["netex"]["namespace"]
+    journeys = params["infomobility"]["journeys"]
 
-    journey_patterns = params["infomobility"]["JourneyPattern"]
+    # Controlla se nel calcolo fermata precedente si fossero trovate più fermate da verificare
+    nearby_stops_id = params["buffer"]["nearby_stops_id"]
+    if nearby_stops_id:
+        lat = params["position_rt"]["latitude"]
+        lon = params["position_rt"]["longitude"]
+        next_stops_distance_avg = []
+        root = ET.parse(netex)
+        # Cerca tutte le fermate successive e calcola la distanza media dalla posizione attuale
+        # una distanza media minore indica che il verso di percorrenza è quello
+        for idx in range(len(nearby_stops_id)):
+            distances = []
+            for sl in root.findall(f".//{ns}ServiceLink"):
+                if sl.find(f"{ns}FromPointRef").get("ref") == nearby_stops_id[idx]:
+                    next_stop_id = sl.find(f"{ns}ToPointRef").get("ref")
+                    stop = root.find(f".//{ns}ScheduledStopPoint[@id='{next_stop_id}']")
+                    lat_stop = float(stop.findtext(f".//{ns}Latitude"))
+                    lon_stop = float(stop.findtext(f".//{ns}Longitude"))
+                    distances.append(calculate_distance(lat, lon, lat_stop, lon_stop))
+            if distances:
+                next_stops_distance_avg.append(sum(distances) / len(distances))
+            else:
+                next_stops_distance_avg.append(-1)
+        if min(next_stops_distance_avg) > 0:
+            index = next_stops_distance_avg.index(min(next_stops_distance_avg))
+            for idx in range(len(journeys)):
+                if journeys[idx]["direction"] == "unbound":
+                    journeys[idx]["stops"].append(nearby_stops_id[index])
+            #params["buffer"]["nearby_stops_id"] = []
+
+    fermate_vicine = neighborhooded_stops(file)
+
+    # Se trova una fermata la si memorizza (sperando sia giusta)
+    if len(fermate_vicine) == 1:
+        for idx in range(len(journeys)):
+            if journeys[idx]["direction"] == "unbound":
+                journeys[idx]["stops"].append(fermate_vicine[0])
+
+    # Se trovano più fermate si cerca di capire il verso del bus
+    # le prossime coordinate gps ce lo diranno quindi per il momento salviamo in buffer le fermate
+    elif len(fermate_vicine) > 1:
+        params["buffer"]["nearby_stops_id"] = fermate_vicine
+
+    # Se non si trovano fermate non si può dire niente
+    # Se comunque resta il dubbio, non si memorizza nessuna fermata
+    
+    open_json(0, file, params)
+
+
+# Funzione per scoprire la fermata collegata al tap
+def calculateStop_due_to_validation(file):
+    params = open_json(1, file)
+    netex = params["netex"]["netex_file_path"] + params["netex"]["netex_file_name"]
+    ns = params["netex"]["namespace"]
+
+    if params["infomobility"]["line_id"] != "":
+        journeys = params["infomobility"]["journeys"]
+
+        nearby_stops_id = neighborhooded_stops(file)
+
+
+        # da lì cercare di capire se è la nostra meta oppure veniamo da lì
+        #else:
+            #fermate = root.findall(f".//{ns}ScheduledStopPoint")
+            #for fermata in fermate:
+                #stop_id = fermata.get("id")
+                #stop_name = fermata.findtext(f"{ns}Name")
+                #lat_stop = float(fermata.findtext(f".//{ns}Latitude"))
+                #lon_stop = float(fermata.findtext(f".//{ns}Longitude"))
+                #distanza = calculate_distance(lat, lon, lat_stop, lon_stop)
+                #if distanza < min_distance:
+                    #min_distance = distanza
+                    #closest_stop = {"id": stop_id, "name": stop_name}
 
 
 ############################################################################################
 # SEZIONE INFOMOBILITà ADMIN
 ############################################################################################
 
-
+# FUNZIONE LEGACY !!!!
 def getInfomobility(file):
     params = open_json(1, file)
     netex = params["netex"]["netex_file_path"] + params["netex"]["netex_file_name"]
     ns = params["netex"]["namespace"]
 
-    journey_patterns = params["infomobility"]["JourneyPattern"]
-    line_id = params["infomobility"]["Line"]["id"]
+    journeys = params["infomobility"]["journeys"]
+    line_id = params["infomobility"]["line_id"]
     
     # Carica il file XML
     root = ET.parse(netex)
 
     #Se è in memoria la linea, elimina le tratte trovate fin ora e aggiungi tutte
     if line_id:
-        journey_patterns = []
+        journeys = []
 
         for sjp in root.findall(f".//{ns}ServiceJourneyPattern"):
             journey = sjp.find(f".//{ns}LineRef[@ref='" + line_id + "']")
@@ -217,7 +214,7 @@ def getInfomobility(file):
                     stop_name = stop.findtext(f"{ns}Description")
                     stops.append({"id": stop_id, "name": stop_name})
 
-                journey_patterns.append({
+                journeys.append({
                     "id": journey_id,
                     "direction": direction,
                     "stops": stops
--- Tap&Go/main.py
+++ Tap&Go/main.py
@@ -3,7 +3,8 @@ from infomobility import *
 jsonfile = "params.json"
 
 #getInfomobility(jsonfile)
-calculateStop_due_to_search(jsonfile)
+#calculateStop_due_to_search(jsonfile)
+calculateStop_due_to_validation(jsonfile)
 #calculateLine(jsonfile)
 
 
