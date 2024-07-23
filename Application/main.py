54bbc0126afd5623ec2b4d67aa917d4a90a16b54
alexannekk <alex.annecchini@gmail.com>
2024-07-23 15:29:32 +0200

    venv fixed

---
+++ Application/main.py
@@ -0,0 +1,11 @@
+from infomobility import *
+
+jsonfile = "params.json"
+
+#getInfomobility(jsonfile)
+#calculateStop(jsonfile)
+#calculateValidation(jsonfile)
+calculateLine(jsonfile)
+
+#asyncio
+
